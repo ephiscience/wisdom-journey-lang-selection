5927d2b94efcb540ff2647fd5987fb135938e18c
Rémi Piotaix <remi@piotaix.fr>
2021-07-02 01:23:37 +0200

    Graceful shutdown

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const morgan = require("morgan");
 
-const app = express();
-app.use(morgan("combined"));
-
 const LANGUAGES = (process.env.LANGUAGES || "en").split(",");
 const DEFAULT_LANGUAGE = "en";
+
+const app = express();
+
+app.use(morgan("combined"));
 
 app.get("/", async (req, res) => {
   const lang = req.acceptsLanguages(LANGUAGES) || DEFAULT_LANGUAGE;
@@ -14,7 +15,9 @@ app.get("/", async (req, res) => {
 });
 
 new Promise((resolve) => {
-  app.listen({ port: 4000 }, () => resolve());
+  const server = app.listen({ port: 4000 }, () => resolve());
+
+  process.on("SIGTERM", () => server.close());
 }).then(() => {
   console.log(`🚀 Server ready`);
 });
